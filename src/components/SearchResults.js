a91f7e220800991a3d339412e56d7df76ce48c17
TrevorTaylor-TP <ttaylor@talentpath.com>
2019-12-02 13:28:28 -0600

    revert to before messing stuff up

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -14,17 +14,9 @@ class SearchResults extends React.Component {
 
   callAPI(location, radius) {
     radius *= 1600;
-    const queryString =
-      "https://thevirtualnomapiproduction.herokuapp.com/search/?location=" +
-      location +
-      "&radius=" +
-      radius;
-    const testString =
-      "http://localhost:9000/search/?location=" +
-      location +
-      "&radius=" +
-      radius;
-    console.log("testString", testString);
+    const queryString = "https://thevirtualnomapiproduction.herokuapp.com/search/?location=" + location+ "&radius=" + radius;
+    const testString = "http://localhost:9000/search/?location=" + location+ "&radius=" + radius;
+    console.log('testString', testString)
     // fetch("https://thevirtualnomapiproduction.herokuapp.com/search/")
     // fetch("http://localhost:9000/search")
     fetch(queryString)
@@ -54,6 +46,7 @@ class SearchResults extends React.Component {
           {json.map((item, i) => {
             return (
               <li key={i}>
+                {/* <RestaurantCard>{item.name}</RestaurantCard> */}
                 <RestaurantCard>{item}</RestaurantCard>
               </li>
             );
